cd8170955e2f9aca39ffcdb5a2f8bf6448d2aeda
jvv00 <jvv0006@gmail.com>
2023-12-26 18:21:24 +0900

    Fix slide

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,20 +1,39 @@
-js;
-var swiper = new Swiper(
-  ".swiper-bestpd",
-
-  {
-    slidesPerView: 2,
-    centeredSlides: true,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  }
-);
-var swiper = new Swiper(".swiper-slide", {
+var swiper1 = new Swiper(".slide", {
   slidesPerView: 1,
+  spaceBetween: 0,
   autoplay: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper2 = new Swiper(".slide-bestpd", {
+  slidesPerView: auto,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var splide = new Splide("#splide-autoscroll", {
+    type: "loop",
+    drag: "free",
+    focus: "center",
+    perPage: 3,
+    autoScroll: {
+      speed: 1,
+    },
+  });
+
+  splide.mount(window.splide.Extensions);
+});
+
+var swiper2 = new Swiper(".slide-gift", {
+  slidesPerView: auto,
+  spaceBetween: 30,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
